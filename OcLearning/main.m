e3ce41df326c81ceaf99b985f5f3aa5b5f2f6972
Haozhe Xu <jamesxuhaozhe@gmail.com>
2017-03-09 21:52:15 -0800

    first commit

---
+++ OcLearning/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  OcLearning
+//
+//  Created by Haozhe Xu on 3/9/17.
+//  Copyright © 2017 Amazon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+    @autoreleasepool {
+        NSLog(@"Programming is fun!");
+    }
+}
